f4c02b54fa150e71b04a4b9737c5fe5fbdde4396
xTheAgentx <davidolbertz@gmail.com>
2024-04-24 21:59:45 +0200

    Initial commit

---
+++ src/main/java/com/theagent/serverliststatuschecker/ServerlistStatusChecker.java
@@ -0,0 +1,23 @@
+package com.theagent.serverliststatuschecker;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "serverliststatuschecker",
+        name = "ServerlistStatusChecker",
+        version = "1.0-SNAPSHOT",
+        authors = {"_The_Agent_"}
+)
+public class ServerlistStatusChecker {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
